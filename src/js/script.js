beb3ec6a3043a7da19ffbc86f086ec69e0e5857e
Yevhen <boikoyevhen@ukr.net>
2023-11-23 11:12:28 +0100

    added email sending

--- src/js/script.js
+++ src/js/script.js
@@ -92,4 +92,30 @@ $(document).ready(function () {
   valideForms("#order form");
   valideForms("#consultation form");
   valideForms("#consultation-form");
+
+  $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+
+    if (!$(this).valid()){
+      return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thx').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
+
 });
